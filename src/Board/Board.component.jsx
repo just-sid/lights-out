e259808eb9fc18e5bab04f5dd7ba0f3f20207b47
just-sid <be10125.17@bitmesra.ac.in>
2020-11-03 23:53:40 +0530

    Added winning title on winning

--- src/Board/Board.component.jsx
+++ src/Board/Board.component.jsx
@@ -42,11 +42,18 @@ class Board extends Component {
         flipCell(y-1, x);
         flipCell(y+1, x);
 
+        let hasWon = board.every(row => row.every(cell => !cell));
         this.setState({
-            board: board});
+            board: board,
+            hasWon: hasWon
+        });
     }
 
     render() {
+
+        if(this.state.hasWon)
+        return <h1>YOU WON</h1>
+
         let tblBoard = [];
         for (let y=0; y < this.props.nrows; y++) {
             let row = [];
