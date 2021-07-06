818fceb1fc8d26edf0f79d74d2b298be8342dea1
priyam125 <sinha.priyam125>
2021-07-07 04:46:09 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ function App() {
   /* let randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex]; */
   return (
+    <div>
+      <nav>
+        <button>Home</button>
+        <button>Rules</button>
+    </nav>
     <div className="App">
       <Board nrows = {5} ncols = {5} chanceLightStartsOn = {0.33}/>
     </div>
+    </div>
+    
   );
 }
 
